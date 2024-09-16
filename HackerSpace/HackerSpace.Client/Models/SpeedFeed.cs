8073bea010b010e32a7f0f5f948b1542ba072235
Robert Garner <rgarner011235@gmail.com>
2024-09-16 13:49:02 -0600

    Created speed and feed calculator

---
+++ HackerSpace/HackerSpace.Client/Models/SpeedFeed.cs
@@ -0,0 +1,46 @@
+﻿namespace HackerSpace.Client.Models
+{
+    public class SpeedFeed
+    {
+        
+        private float rpm;
+        public float Rpm
+        {
+            get { return rpm; }
+            set
+            {
+                rpm = value;
+                Calc();
+            }
+        }
+
+        private int teeth;
+        public int Teeth
+        {
+            get { return teeth; }
+            set
+            {
+                teeth = value;
+                Calc();
+            }
+        }
+        private float chipLoad;
+        public float ChipLoad
+        {
+            get { return chipLoad; }
+            set
+            {
+                chipLoad = value;
+                Calc();
+            }
+        }
+
+        public float FeedRate { get; private set; }
+
+        private void Calc()
+        {
+            FeedRate = rpm * teeth * chipLoad;
+        }
+
+    }
+}
---
+++ HackerSpace/HackerSpace/Data/Models/SpeedFeed.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace HackerSpace.Data.Models
+{
+    public class SpeedFeed
+    {
+        
+        private float rpm;
+        public float Rpm
+        {
+            get { return rpm; }
+            set
+            {
+                rpm = value;
+                Calc();
+            }
+        }
+
+        private int teeth;
+        public int Teeth
+        {
+            get { return teeth; }
+            set
+            {
+                teeth = value;
+                Calc();
+            }
+        }
+        private float chipLoad;
+        public float ChipLoad
+        {
+            get { return chipLoad; }
+            set
+            {
+                chipLoad = value;
+                Calc();
+            }
+        }
+
+        public float FeedRate { get; private set; }
+
+        private void Calc()
+        {
+            FeedRate = rpm * teeth * chipLoad;
+        }
+
+    }
+}
