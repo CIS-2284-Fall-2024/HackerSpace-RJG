48456a359d3fd5b1099192e97258e29241a35672
Robert Garner <rgarner011235@gmail.com>
2024-09-23 14:23:58 -0600

    Added Badges Table

---
+++ Entities/Models/Badge.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities.Models
+{
+    public class Badge
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public string TaskDescription { get; set; }
+        public string SubmissionInstructions { get; set; }
+    }
+}
