782ccbc63fdf274f816af90292d4ac8741a6dc80
Robert Garner <rgarner011235@gmail.com>
2024-10-07 14:31:09 -0600

    Added forms validation

--- Entities/Models/Badge.cs
+++ Entities/Models/Badge.cs
@@ -1,4 +1,6 @@
-﻿namespace Entities.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
 {
     /// <summary>
     /// Represents a hackerspace badge
@@ -6,10 +8,23 @@
     public class Badge
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "You must provide a title!")]
+        [MinLength(3, ErrorMessage = "Must have at least 3 characters.")]
         public string? Title { get; set; }
+
+        [Required]
+        [MinLength(7)]
         public string? Description { get; set; }
+
+        [Required]
+        [MinLength(7)]
         public string? Task { get; set; }
+
+        [Required]
+        [MinLength(7)]
         public string? TurninInstructions { get; set; }
+
         public string? ImagePath { get; set; }
     }
 }
